584b423b37e163620495e116957df96bd9e7a70f
Joseph Jreije <josephjreije35@gmail.com>
2020-10-18 12:01:54 +0300

    Updates

---
+++ precache-manifest.ed4414005c1a679068204fb6044e2149.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9084b1a8bb4f977a1d00e5f120f7a7",
+    "url": "/VPool/index.html"
+  },
+  {
+    "revision": "b94237d647a0720c837d",
+    "url": "/VPool/static/css/2.b9729d07.chunk.css"
+  },
+  {
+    "revision": "d81b405bc797667a1d95",
+    "url": "/VPool/static/css/main.afa0bffe.chunk.css"
+  },
+  {
+    "revision": "b94237d647a0720c837d",
+    "url": "/VPool/static/js/2.c5bcd2ec.chunk.js"
+  },
+  {
+    "revision": "a7a3350ebf64d7b89a77110988161b82",
+    "url": "/VPool/static/js/2.c5bcd2ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d81b405bc797667a1d95",
+    "url": "/VPool/static/js/main.fff3b103.chunk.js"
+  },
+  {
+    "revision": "3b6dc654e5f467cfdd7a",
+    "url": "/VPool/static/js/runtime-main.1b219808.js"
+  },
+  {
+    "revision": "60b2057406950a11632e7c597bcf7096",
+    "url": "/VPool/static/media/VPool.60b20574.jpeg"
+  },
+  {
+    "revision": "0e4981023ce198f0a65e96ab8f4e5cb0",
+    "url": "/VPool/static/media/VPoolLogo.0e498102.png"
+  },
+  {
+    "revision": "f47de7fd81202f36e8fc39bf2a5f296f",
+    "url": "/VPool/static/media/car_red.f47de7fd.png"
+  }
+]);
